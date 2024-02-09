930c2421b04762076a828a39d51e7eeb89097f34
Citoyenbleu <61138831+Citoyenbleu@users.noreply.github.com>
2024-02-09 16:17:02 +0000

    Completed chapter 2.

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -2,8 +2,18 @@
 # Programming challenge: use advanced data collections on the earthquake data
 
 import json
+from collections import Counter
 
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open("30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+events = []
+for event in data["features"]:
+    events.append(event["properties"]["type"])
+
+count = Counter(events)
+
+for key, value in count.items():
+    print(f"{key:15} : {value}")
